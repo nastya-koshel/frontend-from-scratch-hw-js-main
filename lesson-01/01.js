d20d512e3c566c0d327542eaf195978550f10385
Nastya Koshel <nastyakoshel68@gmail.com>
2024-07-31 12:45:47 +0300

    Attempt_2

--- lesson-01/01.js
+++ lesson-01/01.js
@@ -2,11 +2,11 @@
 // Результат присвойте переменной sum
 
 // your code
-{let sum = 0;
+let sum = 0;
 let i = 1;
 
 while (i <= 20) {
     sum = sum + i;
     i = i + 1;
 }
-console.log(sum);}
+console.log(sum);
