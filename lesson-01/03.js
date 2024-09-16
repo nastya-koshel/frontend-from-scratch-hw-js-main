7e1a8a2c36b58d11c172ceb0e42ffcae4a6b17cc
Nastya Koshel <nastyakoshel68@gmail.com>
2024-09-16 14:31:41 +0300

    lesson-01 03 mistake2

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -5,13 +5,13 @@
 */
 
 // your code
-// let sum = 0;
-// let i = 1;
+let sum = 0;
+let i = 1;
 
-// while (i <= 20) {
-//     if (i % 2 !== 0) {
-//         sum = sum + i;
-//         i = i + 1;
-//     }
-// }
-// console.log(sum);
+while (i <= 20) {
+    if (i % 2 !== 0) {
+        sum = sum + i;
+        i = i + 1;
+    }
+}
+console.log(sum);
