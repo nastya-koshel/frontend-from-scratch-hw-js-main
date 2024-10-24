597ef1aa872f0590e4c047310938e8dead81da39
Nastya Koshel <nastyakoshel68@gmail.com>
2024-10-24 22:13:27 +0300

    lesson-04 04

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,12 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+    strArray = str.split("");
+    const doubledArray = []
+    for (let i = 0; i < strArray.length; i++) {
+        doubledArray.push(strArray[i]+strArray[i])
+    }
+    newStr = doubledArray.join("")
+    return newStr
+}
